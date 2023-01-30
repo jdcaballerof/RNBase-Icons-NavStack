76bf78a0680850baa774c3daeb749256ac54a613
David CF <jcaballero@RTP.GOB.MX>
2023-01-30 17:28:31 -0600

    creacion de navStack branch

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import Icon from 'react-native-vector-icons/SimpleLineIcons';
 const App = () => {
   return (
     <View>
-     <Text>App Base </Text>
+     <Text>App Base + navStack </Text>
      <Icon name="mustache" size={30} color="#900" />
    </View>
 )
