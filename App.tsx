d4981b39ec53a55bc82328372a8bf18fdf65f111
David CF <jcaballero@RTP.GOB.MX>
2023-01-30 17:41:34 -0600

    Instalado Nav Stack

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react'
 import { Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 const App = () => {
   return (
-    <View>
-     <Text>App Base + navStack </Text>
-     <Icon name="mustache" size={30} color="#900" />
-   </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+
+        <View style={{backgroundColor: 'hotpink', flex: 1}} >
+          <Text>App Base + navStack </Text>
+          <Icon name="mustache" size={30} color="#900" />
+        </View>
+
+      </NavigationContainer>
+    </GestureHandlerRootView>
 )
 }
 
