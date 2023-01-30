a75f91797c1a32ad1db5739ea2eb427d2330723c
David CF <jcaballero@RTP.GOB.MX>
2023-01-30 17:26:54 -0600

    First Commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
+const App = () => {
+  return (
+    <View>
+     <Text>App Base </Text>
+     <Icon name="mustache" size={30} color="#900" />
+   </View>
+)
+}
+
+export default App;
